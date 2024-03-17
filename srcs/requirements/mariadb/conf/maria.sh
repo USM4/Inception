2d2cbfe3895b11165a821a4be93e45e2c472833d
oredoine <oussamaredoine06@gmail.com>
2024-03-17 14:33:28 +0000

    done with maria and start in wp

--- srcs/requirements/mariadb/conf/maria.sh
+++ srcs/requirements/mariadb/conf/maria.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 service mysql start
+
+#for connection to the socket it takes time (heavy load, establishing connection)
+sleep 2
+
 MYSQL_USER="oredoine"
 MYSQL_PASSWORD="xyz"
-
 DB_NAME="Maria"
 DB_USER="MariaDB"
 DB_PASSWORD="mariamaria"
-MYSQL_ROOT_PWD="123"
-
+MYSQL_ROOT_PWD="abc"
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-# Create a new user and grant privileges
 mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFY BY '$MYSQL_ROOT_PWD'"
 mysql -e "FLUSH PRIVILEGES;"
 
+service mysql stop
 echo "Database configuration completed."
