dca46a3861ffc23e6ca5cca2a79c730c7f77abc7
oredoine <oussamaredoine06@gmail.com>
2024-03-27 02:55:10 +0000

    error in maria while composing all ctnrs

--- srcs/requirements/mariadb/tools/maria.sh
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -2,16 +2,30 @@
 
 service mariadb start
 
-#for connection to the socket it takes time (heavy load, establishing connection)
+# Wait for the database service to be ready
 sleep 10
 
+# Create the database if it doesn't exist
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+
+# Check if the user already exists
+user_exist=$(mysql -e "SELECT user FROM mysql.user WHERE user='$DB_USER';")
+
+# If the user doesn't exist, create it
+if [ -z "$user_exist" ]; then
+    echo "Creating MySQL user '$DB_USER'..."
+    mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+fi
+
+# Grant privileges to the user
+echo "Granting privileges to MySQL user '$DB_USER'..."
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "FLUSH PRIVILEGES;"
 
+# Shutdown the MySQL service
 mysqladmin -u root -p$MYSQL_ROOT_PWD shutdown
-exec mysqld_safe
-echo "Database configuration completed."
 
-# sleep infinity
+# Start MySQL in safe mode
+exec mysqld_safe
+
+echo "Database configuration completed."
