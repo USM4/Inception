47a4e531391451d3563f6ad9d26e17107d7f978f
oredoine <oussamaredoine06@gmail.com>
2024-03-31 14:51:42 +0000

    fixed

--- srcs/requirements/mariadb/tools/maria.sh
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -2,30 +2,16 @@
 
 service mariadb start
 
-# Wait for the database service to be ready
 sleep 10
 
-# Create the database if it doesn't exist
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
+mysql -u root -p"$DB_ROOT_PWD" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -p"$DB_ROOT_PWD" -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -p"$DB_ROOT_PWD" -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -p"$DB_ROOT_PWD" -e "FLUSH PRIVILEGES;"
 
-# Check if the user already exists
-user_exist=$(mysql -e "SELECT user FROM mysql.user WHERE user='$DB_USER';")
+# echo "MySQL root password: $DB_ROOT_PWD"
 
-# If the user doesn't exist, create it
-if [ -z "$user_exist" ]; then
-    echo "Creating MySQL user '$DB_USER'..."
-    mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-fi
-
-# Grant privileges to the user
-echo "Granting privileges to MySQL user '$DB_USER'..."
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "FLUSH PRIVILEGES;"
-
-# Shutdown the MySQL service
-mysqladmin -u root -p$MYSQL_ROOT_PWD shutdown
-
-# Start MySQL in safe mode
-exec mysqld_safe
-
+mysqladmin -u root -p"$DB_ROOT_PWD" shutdown
 echo "Database configuration completed."
+exec mysqld_safe
