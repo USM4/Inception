ce4119b5d4343926522adeca147e636a8850a970
oredoine <oussamaredoine06@gmail.com>
2024-04-05 00:58:54 +0000

    credentials

---
+++ Makefile
@@ -0,0 +1,11 @@
+SRC_DIR = srcs/docker-compose.yml
+
+all:
+	docker-compose -f $(SRC_DIR) up
+
+clean-imgs:
+	docker rmi -f $$(docker image ls)
+clean-containers-volumes:
+	docker rm -f $$(docker ps -aq)
+
+fclean: clean-containers-volumes clean-imgs
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -6,8 +6,6 @@ curl -o wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/pha
 
 sed -i 's|^listen =.*|listen = wordpress:9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
-# echo "clear_env = no" >> /etc/php/7.4/fpm/pool.d/www.conf
-
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
@@ -26,7 +24,7 @@ wp config set DB_HOST 'mariadb:3306' --allow-root --path=/var/www/html
 
 # wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$WP_ADMIN_USER --admin_password=$DB_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path=/var/www/html
 
-wp core install --allow-root --url="https://oredoine.42.fr" --title="refverfdreg" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} \
+wp core install --allow-root --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} \
     --admin_email=${WP_ADMIN_EMAIL} --skip-email --path='/var/www/html'
 
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root --path=/var/www/html
